5e36c3065fb21d1a9ea3be0edffed31cb051f989
Fabiola <fabiola.aurelien@gmail.com>
2026-06-01 17:45:35 -0400

    Wired authRoute to server.js.  Not yet tested

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const PORT = process.env.PORT || 3000;
 
 connectDB();
 
+const authRoutes = require("./routes/authRoutes");
+
+const authRoutes = require("./routes/authRoutes");
 app.use(cors());
 app.use(express.json());
 
