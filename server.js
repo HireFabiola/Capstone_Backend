4cf18cad5d2bcb2004b0252c291c9e6bcafb8a84
Fabiola <fabiola.aurelien@gmail.com>
2026-06-01 15:24:35 -0400

    Added server logic with simple root route message.  NOt yet tested

---
+++ server.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/connection");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("R4B Studio CRM API is running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
