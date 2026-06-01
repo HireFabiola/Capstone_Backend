04bef9d5a62647300604d96e91e1d137518d4806
Fabiola <fabiola.aurelien@gmail.com>
2026-06-01 18:54:30 -0400

    Removed next from pre-save hook

--- models/User.js
+++ models/User.js
@@ -36,18 +36,11 @@ const userSchema = new mongoose.Schema(
 );
 
 // Pre-save middleware to hash the user's password before saving it to the database
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) {
-    return next();
-  }
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
 
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (error) {
-    next(error);
-  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 // Compile the User model from the schema
