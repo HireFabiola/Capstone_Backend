3d349579165e76ba4d0d07f00e0b36964d3c2fec
Fabiola <fabiola.aurelien@gmail.com>
2026-06-01 17:49:10 -0400

    Removed duplicate code causing server crash

--- server.js
+++ server.js
@@ -10,8 +10,7 @@ const PORT = process.env.PORT || 3000;
 connectDB();
 
 const authRoutes = require("./routes/authRoutes");
-
-const authRoutes = require("./routes/authRoutes");
+app.use("/api/auth", authRoutes);
 app.use(cors());
 app.use(express.json());
 
