5105c29978dec18b09c99784283de2d31f0413d1
Fabiola <fabiola.aurelien@gmail.com>
2026-06-01 21:02:06 -0400

    Mounted routes with prefixes server.js

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-require("dotenv").config();
-
 const express = require("express");
 const cors = require("cors");
+
 const connectDB = require("./config/connection");
 
+const authRoutes = require("./routes/authRoutes");
+const inquiryRoutes = require("./routes/inquiryRoutes");
+const projectRoutes = require("./routes/projectRoutes");
+const taskRoutes = require("./routes/taskRoutes");
+
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 connectDB();
 
-const authRoutes = require("./routes/authRoutes");
-
-app.use(express.json());
-app.use("/api/auth", authRoutes);
 app.use(cors());
+app.use(express.json());
 
+app.use("/api/auth", authRoutes);
+app.use("/api/inquiries", inquiryRoutes);
+app.use("/api/projects", projectRoutes);
+app.use("/api/tasks", taskRoutes);
 
-app.get("/", (req, res) => {
-  res.send("R4B Studio CRM API is running");
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log("Server running");
 });
